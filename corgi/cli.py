eab34c6551668bfe7d2fec0fdf72bf62384ab8b0
Kamalpreet Singh <kamalpreet1010@gmail.com>
2021-07-29 14:53:24 +1000

    Added and tested command to launch gitlab

--- corgi/cli.py
+++ corgi/cli.py
@@ -17,4 +17,18 @@ def train():
     Trains a model.
     """
     raise NotImplementedError
+
+@app.command()
+def classify():
+    """
+    Classifies sequences in a Fasta file
+    """
+    raise NotImplementedError
         
+@app.command()
+def repo():
+    """
+    Opens the repository in a web browser
+    """
+    typer.launch("https://gitlab.unimelb.edu.au/mdap/corgi")
+    
