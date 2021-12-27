7cdd55792780eafbd1550fd90d56bb1f0140cb31
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2021-12-27 11:41:16 +1100

    adding optimization stuff - untested yet

--- corgi/optimization.py
+++ corgi/optimization.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import optuna
 from fastai.learner import Learner
 from . import training
@@ -6,28 +7,41 @@ from . import training
 def optimize(
     dls,
     output_dir: (str, Path), # This should be Union
-    learner: Learner = None,
+    n_trials: int,    
+    study_name: str=None,
+    storage_name: str="sqlite:///corgi-studies.db",
+    metric: str="f1_score",
     epochs: int = 20,
     fp16: bool = True,
-    distributed: bool = False,
 ):
+    output_dir = Path(output_dir)
 
     def objective(trial: optuna.Trial):
         # Define parameter space
-
+        lr_max = trial.suggest_float("lr_max", 1e-5, 1e-2, log=True)
+        embedding_dim = trial.suggest_int("embedding_dim", 4, 32)
+        dropout = trial.suggest_float("dropout", 0.0, 1.0)
+        lstm_dims = trial.suggest_int("lstm_dims", 32, 4096, log=True)
+        kernel_size_cnn = trial.suggest_int("kernel_size_cnn", 3, 15, step=2)
 
         # Train
-
-        training.train(
+        learner = training.train(
             dls,
-            output_dir,
-            learner
+            output_dir=output_dir/f"trial-{trial.trial_id}",
+            fp16=fp16,
+            epochs=epochs,
+            lr_max=lr_max,
+            dropout=dropout,
+            embedding_dim=embedding_dim,
+            lstm_dims=lstm_dims,
+            kernel_size_cnn=kernel_size_cnn,
         )
 
         # Return metric from recorder
-        return learner.recorder
+        metric_value = max(map(lambda row: row.get(metric), learner.recorder.values))
+        return metric_value
 
-    study = optuna.create_study()
-    study.optimize(objective)
+    study = optuna.create_study(study_name=study_name, storage=storage_name, load_if_exists=True)
+    study.optimize(objective, n_trials=n_trials)
     return study
 
